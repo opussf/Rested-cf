ba6a5b563775003331dc1fb862e0883370da64bf
opussf <github@zz9-za.com>
2024-12-08 13:17:40 -0800

    3.18.1

--- RestedGuild.lua
+++ RestedGuild.lua
@@ -3,17 +3,19 @@
 --=================
 table.insert( Rested.filterKeys, "guildName" )
 function Rested.SaveGuildInfo( ... )
-	--Rested.Print("PLAYER_GUILD_UPDATE")
+	-- Rested.Print("PLAYER_GUILD_UPDATE")
 	local gName, gRankName, gRankIndex = GetGuildInfo("player")
-	Rested_restedState[Rested.realm][Rested.name].guildName = gName or nil
-	Rested_restedState[Rested.realm][Rested.name].guildRank = gName and gRankName or nil
-	Rested_restedState[Rested.realm][Rested.name].guildRankIndex = gName and gRankIndex or nil
+	Rested.me.guildName = gName or nil
+	Rested.me.guildRank = gName and gRankName or nil
+	Rested.me.guildRankIndex = gName and gRankIndex or nil  -- gRankIndex is the index of the rank
+
 	local rep, bottom, top, reaction = Rested.GetGuildRep()
 	bottom = 0
 	--rep = rep - bottom; top = top - bottom; bottom = 0
-	Rested_restedState[Rested.realm][Rested.name].guildRep = gName and rep or nil
-	Rested_restedState[Rested.realm][Rested.name].guildBottom = gName and bottom or nil
-	Rested_restedState[Rested.realm][Rested.name].guildTop = gName and top or nil
+	Rested.me.guildRep = gName and rep or nil
+	Rested.me.guildBottom = gName and bottom or nil
+	Rested.me.guildTop = gName and top or nil
+	Rested.me.guildReaction= gName and reaction or nil  -- reaction is the FactionStandingLabel index.
 	--Rested.Print(string.format("%s :: %i - %i - %i", gName or "None", bottom, rep, top))
 end
 function Rested.GetGuildRep( )
@@ -29,6 +31,7 @@ Rested.EventCallback( "PLAYER_GUILD_UPDATE", Rested.SaveGuildInfo )
 
 Rested.dropDownMenuTable["Guild"] = "guild"
 Rested.commandList["guild"] = {["help"] = {"","Show guild standing"}, ["func"] = function()
+		Rested.SaveGuildInfo()
 		Rested.reportName = "Guild Standing"
 		Rested.UIShowReport( Rested.GuildStandingReport )
 	end
@@ -40,7 +43,7 @@ function Rested.GuildStandingReport( realm, name, charStruct )
 		lineCount = 1
 		Rested.strOut = string.format( "%s :%s: %s",
 				charStruct.guildName,
-				(charStruct.guildRankIndex and _G["FACTION_STANDING_LABEL"..charStruct.guildRankIndex] or ""),
+				(charStruct.guildReaction and _G["FACTION_STANDING_LABEL"..charStruct.guildReaction] or ""),
 				rn )
 		table.insert( Rested.charList,
 				{ ( ( charStruct.guildRep or 0 ) / ( ( ( charStruct.guildTop or 0 ) - ( charStruct.guildBottom or 0 ) ) + 1 ) ) * 150,
