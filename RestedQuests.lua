5dbf58d663fdaf1708b89eeb8b01d02e991cb126
opussf <github@zz9-za.com>
2025-08-14 12:03:50 -0700

    3.24

--- RestedQuests.lua
+++ RestedQuests.lua
@@ -10,10 +10,12 @@ function Rested.QuestCommand( strIn, retryCount )
 				-- Rested.Print( "Checking quest: "..qnum )
 				title = C_QuestLog.GetTitleForQuestID( qnum )
 				completed = C_QuestLog.IsQuestFlaggedCompleted( qnum )
+				onquest = C_QuestLog.IsOnQuest( qnum )
 				Rested.me.quests = Rested.me.quests or {}
 				Rested.me.quests[qnum] = {
 						["title"] = title or "Unknown Name",
 						["completed"] = completed,
+						["onquest"] = onquest,
 						["addedTS"] = time() + qcount,
 						["completedTS"] = ( completed and time() or nil ),
 					}
@@ -37,7 +39,7 @@ function Rested.QuestReport( realm, name, charStruct )
 			else
 				table.insert( Rested.charList, { time() - (qinfo.completed and qinfo.completedTS or qinfo.addedTS),
 						string.format( "%6i: %s :: %s",
-							qnum, (qinfo.completed and "COMPLETE" or "progress"), qinfo.title ) } )
+							qnum, (qinfo.completed and "COMPLETE" or (qinfo.onquest and "in progress" or "future")), qinfo.title ) } )
 				count = count + 1
 			end
 		end
@@ -52,6 +54,9 @@ function Rested.QuestUpdate()
 			if not qinfo.completed and C_QuestLog.IsQuestFlaggedCompleted( qnum ) then
 				Rested.me.quests[qnum].completed = true
 				Rested.me.quests[qnum].completedTS = time()
+			elseif not qinfo.onquest and C_QuestLog.IsOnQuest( qnum ) then
+				Rested.me.quests[qnum].onquest = true
+				Rested.me.quests[qnum].addedTS = time()
 			elseif qinfo.completed and qinfo.completedTS < time() - 160 then  -- completd more than 5 minutes ago.
 				Rested.me.quests[qnum] = nil
 			end
@@ -76,3 +81,83 @@ Rested.reportReverseSort["Quests"] = true
 Rested.EventCallback( "QUEST_LOG_UPDATE", Rested.QuestUpdate )
 -- Rested.EventCallback( "QUEST_ACCEPTED", Rested.QuestCommand )
 Rested.EventCallback( "QUEST_REMOVED", Rested.QuestRemoved )
+
+-- storylines
+function Rested.QuestStoryline( strIn )
+	if strIn and strIn ~= '' then
+		for slnum in string.gmatch( strIn, "([0-9]+)[,]*" ) do
+			local questNums = C_QuestLine.GetQuestLineQuests( slnum )
+			Rested.QuestCommand( table.concat( questNums, "," ) )
+		end
+	end
+end
+Rested.commandList["storylines"] = { ["help"] = {"[storyline,...]","Track quests in a storyline"}, ["func"] = Rested.QuestStoryline }
+
+
+--[[
+
+/rested quests 84967,86835,84965,84964,84963,84961,84960,84959,84958,85039,85003,84957,84956
+/rested quests 84826,84827,84831,85730,86327,84834,84869,84838,84848,84867,86332,84876,84879,84883,84910
+/rested quests 90517,86946,84866,84865,84864,84863,84862,84861,84860,84859,84858,84857,84856,86495,84855,85961,85032
+/rested quests 85037,84906,84905,84904,84903,84902,84900,84899,84898,84897,
+/rested quests 86820
+
+/rested storylines 5690,5717,5696,5734,5733,5780
+
+
+C_QuestLog.IsOnQuest(questID)
+C_QuestLog.IsComplete(questID)
+
+
+
+local questLines = C_QuestLine.GetAvailableQuestLines(uiMapID)
+for _, ql in ipairs(questLines) do
+    print(ql.questLineName, ql.questLineID)
+end
+
+each ql:
+{
+    questLineName = string,
+    questLineID = number,
+    questID = number,       -- first quest in the line
+    isCampaign = boolean,
+    isHidden = boolean
+}
+
+local uiMapID = 84 -- Stormwind City
+local questLines = C_QuestLine.GetAvailableQuestLines(uiMapID)
+
+for _, ql in ipairs(questLines) do
+    print("Storyline:", ql.questLineName)
+    local quests = C_QuestLine.GetQuestLineQuests(ql.questLineID, uiMapID)
+    for _, quest in ipairs(quests) do
+        print(" -", quest.questName, "(ID:", quest.questID .. ")")
+    end
+end
+
+----------------------------
+
+C_CampaignInfo.GetCampaignChapterInfo(campaignChapterID)
+C_CampaignInfo.GetCampaignID(questID)
+
+
+C_QuestLine.GetQuestLineQuests(questLineID)
+C_QuestLine.IsComplete(questLineID)
+
+C_QuestLine.GetQuestLineQuests(5690)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]]
